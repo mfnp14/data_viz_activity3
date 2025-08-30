db609940dd46c2b41180c9ac69200f6b2edff8c1
Marcel Pontes <66149732+mfnp14@users.noreply.github.com>
2025-08-30 16:46:42 -0300

    Adição de filtro por data e dia e criação de gráfico de barras (#1)
    Description:
    - Adição de filtro por data e dia na barra lateral do dashboard e de um gráfico de barras do total de registros por mês do ano.
    
    Co-authored-by: Marcel Pontes <mpontes@apple.com>

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from utils import df
-
+from utils import *
 st.set_page_config(layout='wide')
 
 st.title("Atividade 3 - Credit Card Spending in India (Dashboard)")
@@ -8,3 +7,38 @@ st.write("Link: https://www.kaggle.com/datasets/divyaraj2006/credit-card-spendin
 st.image("assets/course_banner.png")
 
 st.write(df.head())
+
+#------------------------------------------------------------
+
+st.set_page_config(layout='wide')
+st.sidebar.write("Escolha as páginas")
+st.sidebar.markdown("## Seleções no Gráfico")
+
+select_event = st.sidebar.multiselect('Selecione datas', options=date_options)
+select_city = st.sidebar.selectbox('Selecione a cidade', options=city_options, index=0)
+
+# Filtrando período
+if select_event:
+    filtered = filtered[filtered['Date'].dt.normalize().isin(select_event)]
+# Filtrando Cidades
+if select_city != 'Todas':
+    filtered = filtered[filtered['City'] == select_city]
+
+st.write(f'Linhas após filtro: {len(filtered)}')
+st.dataframe(filtered.head(20))
+
+#------------------------------------------------------------
+
+grouped_data = (
+    filtered
+    .assign(Date=filtered['Date'].dt.normalize())
+    .groupby('Date', as_index=False)['Amount']
+    .sum()
+    .sort_values('Date')
+)
+
+if grouped_data.empty:
+    st.info('Não há dados selecionados')
+else:
+    chart_df = grouped_data.set_index('Date')
+    st.bar_chart(data=chart_df, use_container_width=True)
--- utils.py
+++ utils.py
@@ -3,4 +3,14 @@ import pandas as pd
 
 path = kagglehub.dataset_download("divyaraj2006/credit-card-spending-in-india")
 df = pd.read_csv(f"{path}/Credit Card Spending in India.csv")
+df = df.dropna(how="all")
 # --------------------------------------------------------------
+
+#-- Selecionar o periodo de coleta
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+df['Amount'] = pd.to_numeric(df['Amount'], errors='coerce')
+
+date_options = sorted(df['Date'].dt.normalize().unique())
+city_options = ['Todas'] + sorted(df['City'].dropna().unique().tolist())
+
+filtered = df.copy()
