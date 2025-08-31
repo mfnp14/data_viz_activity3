fb585a6adb9098febcb01502367a848187017401
Marcel Pontes <66149732+mfnp14@users.noreply.github.com>
2025-08-31 00:26:52 -0300

    Criação de pie chart e boxplot (#3)
    Descrição:
    - Criação de um pie chart e um boxplot dentro de uma área de tabs.
    
    Co-authored-by: Marcel Pontes <mpontes@apple.com>

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
+import plotly.express as px
 from utils import *
 st.set_page_config(layout='wide')
 
 st.title("Atividade 3 - Credit Card Spending in India (Dashboard)")
 st.write("Link: https://www.kaggle.com/datasets/divyaraj2006/credit-card-spending-in-india")
 st.image("assets/course_banner.png")
-st.image("assets/dataset-cover.jpg")
-
-#st.write(df.head())
+st.image("assets/dataset-cover.jpg", width=400)
 
 #------------------------------------------------------------
 
@@ -17,7 +16,8 @@ st.sidebar.markdown("## Seleções no Gráfico")
 
 select_event = st.sidebar.multiselect('Selecione datas', options=date_options)
 select_city = st.sidebar.selectbox('Selecione a cidade', options=city_options, index=0)
-select_gender = st.sidebar.selectbox('Selecione a Gênero', options=gender_options, index=0)
+select_gender = st.sidebar.selectbox('Selecione o gênero', options=gender_options, index=0)
+select_card_type = st.sidebar.radio('Selecione um tipo de cartão', card_options)
 
 #------------------------------------------------------------
 
@@ -27,6 +27,11 @@ if select_event:
 # Filtrando Cidades
 if select_city != 'Todas':
     filtered = filtered[filtered['City'] == select_city]
+# Filtrando por tipo de cartão
+if select_card_type != 'Todos':
+    filtered_card = filtered[filtered['Card Type'] == select_card_type]
+else:
+    filtered_card = filtered
 
 # -----------------------------------------------------------
 # CARDS TOTAIS
@@ -55,8 +60,31 @@ st.dataframe(filtered.head(20))
 
 #------------------------------------------------------------
 
+
+tab1, tab2 = st.tabs(["Pie chart", "Box plot"])
+with tab1:
+    st.subheader("Gráfico dos tipos de cartão (por cidade)")
+    card_type_count = filtered["Card Type"].value_counts()
+    fig1 = px.pie(card_type_count, values="count", names=card_type_count.index, color=card_type_count.index, color_discrete_map=card_colors)
+    st.plotly_chart(fig1, key="pie_chart")
+
+with tab2:
+    st.subheader("Box plot de custo de transação (por tipo de cartão)")
+    fig2 = px.box(
+        filtered_card,
+        x="Card Type",
+        y="Amount",
+        color="Card Type",
+        color_discrete_map=card_colors,
+        points="all",
+        labels={"Amount": "Valor ($)", "Card Type": "Tipo de Cartão"},
+    )
+    st.plotly_chart(fig2, key="box_plot")
+
+#------------------------------------------------------------
+
 with st.container():
-    st.write("Gráfico Histórico de Gastos")
+    st.subheader("Gráfico Histórico de Gastos (por data e cidade)")
 grouped_data = (
     filtered
     .assign(Date=filtered['Date'].dt.normalize())
